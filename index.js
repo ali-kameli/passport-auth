52fba9fc06ef67f1ae962019bf9b5672b62af40a
alikameli <alikameli0123@gmail.com>
2023-08-14 09:37:20 +0330

    flase message and session

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 const { default: mongoose } = require('mongoose');
 const { AllRoutes } = require('./routes/index.routes');
+const flash = require('express-flash');
+const session = require('express-session');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/passport-auth', {}).then(() => console.log(`connected to MongoDB...`)).catch(err => console.log('have problem to connect MongoDB !!'));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(flash());
+app.use(session({
+    secret: 'secret key',
+    resave: false, //! always false
+    saveUninitialized: false
+}))
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
